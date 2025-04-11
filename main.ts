46e2a5cf2fcb12c46bc265b43397f965cc71bd2c
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2025-04-11 13:00:28 +0200

    Initial

--- main.ts
+++ main.ts
@@ -10,5 +10,18 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import { kvOAuthPlugin } from "kv_oauth/fresh.ts";
 import { oauth2Client } from "./utils/kv_oauth.ts";
+import { kvInsightsPlugin } from 'https://deno.land/x/deno_kv_insights@v0.8.4-beta/mod.ts';
 
-await start(manifest, { plugins: [kvOAuthPlugin(oauth2Client)] });
+const connectionUrl = Deno.env.get("DENO_DEPLOYMENT_ID")
+    ? undefined // Use Deno Deploy KV in production
+    : Deno.env.get("DENO_KV_CONNECT_URL");
+  
+// Open the KV connection
+const kv = await Deno.openKv(connectionUrl);
+
+await start(manifest, { 
+  plugins: [
+    kvOAuthPlugin(oauth2Client),
+    kvInsightsPlugin({ kv })
+  ]
+});
